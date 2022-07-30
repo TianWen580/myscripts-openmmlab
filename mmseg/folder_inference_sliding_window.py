ed028f63acec471c50cf328fd6ffa2eb873303e8
布鲁瓦丝 <62822224+TianWen580@users.noreply.github.com>
2022-07-30 09:29:42 +0800

    Update folder_inference_sliding_window.py

--- mmseg/folder_inference_sliding_window.py
+++ mmseg/folder_inference_sliding_window.py
@@ -6,19 +6,19 @@ from PIL import Image
 from mmseg.apis import inference_segmentor, init_segmentor, show_result_pyplot
 from mmseg.core.evaluation import get_palette
 
-test_dir = ''
+test_dir = '' # 待预测文件所在文件夹
 windowSize = 512 # 预测滑窗大小
 
-config_dir = 'configs/xxx/xxx.py'
-checkpoint_dir = '~/xxx.pth'
-device = 'cuda:0'
+config_dir = 'configs/xxx/xxx.py' # 模型配置文件
+checkpoint_dir = '~/xxx.pth' # 模型权值文件
+device = 'cuda:0' # 推理设备
 
 
 def sliding_pred(img_dir, crop_size=256):
-          img = cv2.imread(img_dir, flags=-1)
+    img = cv2.imread(img_dir, flags=-1)
     h, w = img.shape[:2]
-    num_h = h // crop_size
-    num_w = w // crop_size
+    num_h = h // crop_size # 滑窗水平方向个数
+    num_w = w // crop_size # 滑窗竖直方向个数
     img = np.array(img)
     img_crop = np.zeros((crop_size, crop_size, 3))
 
@@ -38,10 +38,10 @@ def sliding_pred(img_dir, crop_size=256):
                 a_row = np.concatenate(pred_result0, axis=1)
                 pred_result0 = []
             pass
-        rows.append(a_row)
+        rows.append(a_row) # 纵向拼接
         pass
     pred_png = np.concatenate(rows, axis=0)
-    pred_png = pred_png[:3821,:3752]
+    pred_png = pred_png[:3821,:3752] # 根据待预测大图的规格裁切多余黑边区域
 
     a_list = [pred_png]
     
@@ -55,5 +55,5 @@ def sliding_pred(img_dir, crop_size=256):
 if __name__ == "__main__":
     files = os.listdir(test_dir)
 
-                for f in tqdm(files):
-                    sliding_pred(os.path.join(test_dir, f), crop_size=windowSize)
+    for f in tqdm(files):
+        sliding_pred(os.path.join(test_dir, f), crop_size=windowSize)
